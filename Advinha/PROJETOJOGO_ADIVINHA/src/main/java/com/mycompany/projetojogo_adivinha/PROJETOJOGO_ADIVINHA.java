755a7333cd56310f9c3ad9cecd6284974e47e37e
Antony-Rya <159479364+Antony-Rya@users.noreply.github.com>
2025-01-29 15:58:14 -0300

    👍
    Adicionei a interface inicial

---
+++ Advinha/PROJETOJOGO_ADIVINHA/src/main/java/com/mycompany/projetojogo_adivinha/PROJETOJOGO_ADIVINHA.java
@@ -0,0 +1,7 @@
+package com.mycompany.projetojogo_adivinha;
+public class PROJETOJOGO_ADIVINHA {
+    public static void main(String[] args) {
+        JFHome home = new JFHome();
+        home.setVisible(true);
+    }
+}
